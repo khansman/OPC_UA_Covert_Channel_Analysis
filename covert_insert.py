4ae5f180a0a725a45f3111e501248e626b8230a6
khansman <kaihansmann@t-online.de>
2023-02-17 17:54:47 -0500

    covert_insert.py first version

---
+++ covert_insert.py
@@ -0,0 +1,22 @@
+
+from netfilterqueue import NetfilterQueue
+from subprocess import DEVNULL, STDOUT, call
+
+def print_and_accept(pkt):
+    pkt.retain()
+    pl = pkt.get_payload()
+    print(pkt)
+    pkt.accept()
+
+
+nfqueue = NetfilterQueue()
+nfqueue.bind(1, print_and_accept)
+try:
+    call(['sudo iptables -D OUTPUT -p tcp --sport 4840 -j NFQUEUE --queue-num 1'], shell=True, stdout=DEVNULL, stderr=STDOUT)
+    call(['sudo iptables -I OUTPUT -p tcp --sport 4840 -j NFQUEUE --queue-num 1'], shell=True, stdout=DEVNULL, stderr=STDOUT)
+    nfqueue.run()
+except KeyboardInterrupt:
+    call(['sudo iptables -D OUTPUT -p tcp --sport 4840 -j NFQUEUE --queue-num 1'], shell=True, stdout=DEVNULL, stderr=STDOUT)
+    print("")
+
+nfqueue.unbind()
