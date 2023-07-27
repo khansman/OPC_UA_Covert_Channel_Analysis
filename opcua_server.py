75a213f253fe6b3bb8f3a7d7dffb5a668fb9e534
khansman <50320727+khansman@users.noreply.github.com>
2023-07-27 10:14:13 -0400

    Add files via upload

--- opcua_server.py
+++ opcua_server.py
@@ -1,114 +1,103 @@
 import getopt
+import string
 import sys
 from opcua import Server
 from time import sleep
 import functions
 
-temperate_old = 0
-windspeed_old = 0
-temperate = 0
-windspeed = 0
-counter = 1
-ip = ""
 
-if len(sys.argv) > 1:
-    argv = sys.argv[1:]
-    opts, args = getopt.getopt(argv, "hi:", ["help=", "ip="])
-    for opt, arg in opts:
-        if opt in ("-h", "--help"):
-            print(
-                "\n Usage: \n\t opcua_server.py [-i, --ip] = 127.0.0.1 \n\t opcua_server.py [-h, --help]\n")
-            sys.exit(0)
-        elif opt in ("-i", "--ip"):
-            ip = arg
-else:
-    print(
-        "\n Usage: \n\t opcua_server.py [-i, --ip] = 127.0.0.1 \n\t opcua_server.py [-h, --help]\n")
-    sys.exit()
+def server_setup():
+    sv = Server()
+    sv.set_server_name("OpcUa Test Server")
+    sv_endpoint = "opc.tcp://" + ip + ":4840"
+    sv.set_endpoint(sv_endpoint)
 
-server = Server()
-server.set_server_name("OpcUa Test Server")
-server_endpoint = "opc.tcp://" + ip + ":4840"
-server.set_endpoint(server_endpoint)
+    # ENCRYPTION SETUP
 
-# ENCRYPTION SETUP
+    # server.set_security_policy([ua.SecurityPolicyType.Basic256Sha256_SignAndEncrypt])
+    # server.load_certificate("certificate.pem")
+    # server.load_private_key("key.pem")
 
-# server.set_security_policy([ua.SecurityPolicyType.Basic256Sha256_SignAndEncrypt])
-# server.load_certificate("certificate.pem")
-# server.load_private_key("key.pem")
+    sv.register_namespace("Station1")
+    return sv
 
 
-server.register_namespace("Station1")
-objects = server.get_objects_node()
+def sensor_declaration(sv):
+    objects = sv.get_objects_node()
+    '''
+    Object 1:  
+        - temperature sensor:
+        - 4 variables
+                - vendor Name (not writable)
+                - serial Number (not writable)
+                - temperature (WRITABLE)
+                - state of danger (WRITABLE)
+    '''
+    objectstation_temp = objects.add_object('n=2;s="TS1"', "TempSensor1")
+    tempsens_vn = objectstation_temp.add_variable('n=2;s="S1_VendorName"', "TS1 Vendor Name", "Sensor VULKAN")
+    tempsens_sn = objectstation_temp.add_variable('n=2;s="S1_SerialNumber"', "TS1 Serial Number", 12345678)
+    tempsens_temp = objectstation_temp.add_variable('n=2;s="S1_Temperature"', "TS1 Temperature", 15)
+    tempsens_temp.set_writable()
+    tempsens_danger = objectstation_temp.add_variable('n=2;s="S1_Danger"', "TS1 State of Danger", False)
+    tempsens_danger.set_writable()
+    temp_data = [tempsens_vn, tempsens_sn, tempsens_temp, tempsens_danger]
 
-'''
-Object 1:  
-    - temperature sensor:
-    - 4 variables
-            - vendor Name (not writable)
-            - serial Number (not writable)
-            - temperature (WRITABLE)
-            - state of danger (WRITABLE)
-'''
-objectStation_Temp = objects.add_object('n=2;s="TS1"', "TempSensor1")
-tempsens_vn = objectStation_Temp.add_variable('n=2;s="S1_VendorName"', "TS1 Vendor Name", "Sensor VULKAN")
-tempsens_sn = objectStation_Temp.add_variable('n=2;s="S1_SerialNumber"', "TS1 Serial Number", 12345678)
-tempsens_temp = objectStation_Temp.add_variable('n=2;s="S1_Temperature"', "TS1 Temperature", 15)
-tempsens_temp.set_writable()
-tempsens_danger = objectStation_Temp.add_variable('n=2;s="S1_Danger"', "TS1 State of Danger", False)
-tempsens_danger.set_writable()
+    '''
+    Object 2:  
+        - wind sensor:
+        - 3 variables
+                - vendor Name (not writable)
+                - serial Number (not writable)
+                - windspeed (WRITABLE)
+    '''
+    objectstation_wind = objects.add_object('n=2;s="WS1"', "WindSensor1")
+    windsens_vn = objectstation_wind.add_variable('n=2;s="WS1_VendorName"', "WS1 Vendor Name", "Sensor STORM")
+    windsens_sn = objectstation_wind.add_variable('n=2;s="WS1_SerialNumber"', "WS1 Serial Number", 98765432)
+    windsens_speed = objectstation_wind.add_variable('n=2;s="WS1_Windspeed"', "WS1 Windspeed", 11)
+    windsens_speed.set_writable()
+    wind_data = [windsens_vn, windsens_sn, windsens_speed]
 
-'''
-Object 2:  
-    - wind sensor:
-    - 3 variables
-            - vendor Name (not writable)
-            - serial Number (not writable)
-            - windspeed (WRITABLE)
-'''
-objectStation_Wind = objects.add_object('n=2;s="WS1"', "WindSensor1")
-windsens_vn = objectStation_Wind.add_variable('n=2;s="WS1_VendorName"', "WS1 Vendor Name", "Sensor STORM")
-windsens_sn = objectStation_Wind.add_variable('n=2;s="WS1_SerialNumber"', "WS1 Serial Number", 98765432)
-windsens_speed = objectStation_Wind.add_variable('n=2;s="WS1_Windspeed"', "WS1 Windspeed", 11)
-windsens_speed.set_writable()
+    return temp_data, wind_data
 
-print("\n [*] Starting Server!\n")
-server.start()
-print("\n [*] Server online!\n")
 
-try:
-    data_mode = input("\t Please enter the mode of data generation (linear,random,sinus,cosinus): ")
-    print("\n\t Updating Data ...")
-    if data_mode == "linear":
+def sensor_initialisation(mode, temp_data, wind_data):
+    temperate_old = 0
+    windspeed_old = 0
+    counter = 1
+
+    tempsens_temp = temp_data[2]
+    windsens_speed = wind_data[2]
+
+    if mode == "linear":
         while True:
             temperature, temperate_old = functions.linear(temperate_old)
             tempsens_temp.set_value(temperature)
             windspeed, windspeed_old = functions.linear(windspeed_old)
             windsens_speed.set_value(windspeed)
-            sys.stdout.write("\r\t Temperature: {} ".format(round(temperature, 3)) + "\t Windspeed: {}".format(
-                round(windspeed, 3)) + "\t Total Updates: {}".format(counter))
+            sys.stdout.write("\r\t Temperature: {} ".format("{:.3f}".format(round(temperature, 3))) + "\t Windspeed: {}".format(
+                "{:.3f}".format(round(windspeed, 3))) + "\t Total Updates: {}".format(counter))
             sys.stdout.flush()
             counter += 1
             sleep(0.2)
-    elif data_mode == "random":
+    elif mode == "random":
         while True:
             temperature, temperate_old = functions.random_value(27, 100)
             tempsens_temp.set_value(temperature)
             windspeed, windspeed_old = functions.random_value(11, 50)
             windsens_speed.set_value(windspeed)
-            sys.stdout.write("\r\t Temperature: {} ".format(round(temperature, 3)) + "\t Windspeed: {}".format(
-                round(windspeed, 3)) + "\t Total Updates: {}".format(counter))
+            sys.stdout.write("\r\t Temperature: {} ".format("{:.3f}".format(round(temperature, 3))) + "\t Windspeed: {}".format(
+                "{:.3f}".format(round(windspeed, 3))) + "\t Total Updates: {}".format(counter))
             sys.stdout.flush()
             counter += 1
             sleep(0.2)
-    elif data_mode == "sinus":
+    elif mode == "sinus":
         while True:
             temperature, temperate_old = functions.sinus(temperate_old)
             tempsens_temp.set_value(temperature)
             windspeed, windspeed_old = functions.cosinus(windspeed_old)
             windsens_speed.set_value(windspeed)
-            sys.stdout.write("\r\t Temperature: {} ".format(round(temperature, 3)) + "\t Windspeed: {}".format(
-                round(windspeed, 3)) + "\t Total Updates: {}".format(counter))
+            sys.stdout.write("\r\t Temperature: {} ".format("{:.3f}".format(round(temperature, 3))) + "\t Windspeed: {}".format(
+                "{:.3f}".format(round(windspeed, 3))) + "\t Total Updates: {}".format(counter))
             sys.stdout.flush()
             counter += 1
             sleep(0.2)
@@ -118,8 +107,8 @@ try:
             tempsens_temp.set_value(temperature)
             windspeed, windspeed_old = functions.sinus(windspeed_old)
             windsens_speed.set_value(windspeed)
-            sys.stdout.write("\r\t Temperature: {} ".format(round(temperature, 3)) + "\t Windspeed: {}".format(
-                round(windspeed, 3)) + "\t Total Updates: {}".format(counter))
+            sys.stdout.write("\r\t Temperature: {} ".format("{:.3f}".format(round(temperature, 3))) + "\t Windspeed: {}".format(
+                "{:.3f}".format(round(windspeed, 3))) + "\t Total Updates: {}".format(counter))
             sys.stdout.flush()
             counter += 1
             sleep(0.2)
@@ -127,6 +116,34 @@ try:
         print("Please execute the program again and enter a valid data mode!")
         sys.exit()
 
-except KeyboardInterrupt:
-    server.stop()
-    print("\n\n [*] Server offline!\n")
+
+if __name__ == "__main__":
+
+    if len(sys.argv) > 1:
+        argv = sys.argv[1:]
+        opts, args = getopt.getopt(argv, "hi:", ["help=", "ip="])
+        for opt, arg in opts:
+            if opt in ("-h", "--help"):
+                print(
+                    "\n Usage: \n\t opcua_server.py [-i, --ip] = 127.0.0.1 \n\t opcua_server.py [-h, --help]\n")
+                sys.exit(0)
+            elif opt in ("-i", "--ip"):
+                ip = arg
+    else:
+        print(
+            "\n Usage: \n\t opcua_server.py [-i, --ip] = 127.0.0.1 \n\t opcua_server.py [-h, --help]\n")
+        sys.exit()
+
+    server = server_setup()
+    temp_sensor, wind_sensor = sensor_declaration(server)
+    try:
+        print("\n [*] Starting Server!\n")
+        server.start()
+        print("\n [*] Server online!\n")
+
+        data_mode = input("\t Please enter the mode of data generation (linear,random,sinus,cosinus): ")
+        print("\n\t Updating Data ...")
+        sensor_initialisation(data_mode, temp_sensor, wind_sensor)
+    except KeyboardInterrupt:
+        server.stop()
+        print("\n\n [*] Server offline!\n")
